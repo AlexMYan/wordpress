f61d96df31a17e155e6f73239d839f440e2cb00b
yanovicham <yanovich@newit.by>
2022-12-07 12:17:06 +0300

    master

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php //Толькло для теста
+//header('Content-Type: application/json; charset=utf-8');
+require_once(__DIR__ . '/wp-load.php');
+//Вспомогательный класс
+$classExtra = new \Helper\Extra();
+//Вспомогательный класс
+$classListForms = new \Helper\ListForms();
+
+$classSearchFilter = new \Helper\SearchFilter();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>HTML Document</title>
+</head>
+<body>
+
+<?php
+
+
+
+?>
+
+</body>
+</html>
